a2b695a6a8b2cc91707477ca720572ca9de801af
Kamin Day <kaminday@gmail.com>
2017-09-22 10:26:15 -0500

    Fixed indentation on build.rb and added file description

--- build.rb
+++ build.rb
@@ -2,6 +2,7 @@
 
 # TODO: Indentation is messed up. Fix please.
 
+# Build.rb is a file used to populate the videos table of the 'netflix' database with data which is read from a CSV file. 
 require 'rubygems'
 require 'bundler'
 
@@ -9,22 +10,14 @@ Bundler.require
 
 require "csv"
 
+# Sets utility to open the correct database.
 conn = PGconn.open(:dbname => 'netflix')
 
-
-	CSV.foreach("NetflixDatabase.csv", {headers: true, return_headers: false}) do |row|
-		 
-		title = row["title"].chomp
-		description = row["description"].chomp
-		video = row["video"].chomp
-		views = row["views"].to_i
-	
-
-		result = conn.exec("INSERT INTO videos (title, description, video, views) VALUES('"+title.to_s+"', '" +description.to_s+"', '"+video+"', #{views});")
-	end
-
-
- # id | title | description | video | views 
- # '
- # '
- # INSERT INTO users (name, password) VALUES('kamin', 'pw');
+# This reads through the CSV file and adds the data to the videos table.
+CSV.foreach("NetflixDatabase.csv", {headers: true, return_headers: false}) do |row|	 
+	title = row["title"].chomp
+	description = row["description"].chomp
+	video = row["video"].chomp
+	views = row["views"].to_i
+	result = conn.exec("INSERT INTO videos (title, description, video, views) VALUES('"+title.to_s+"', '" +description.to_s+"', '"+video+"', #{views});")
+end
