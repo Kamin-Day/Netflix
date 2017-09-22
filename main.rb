c0b3ea2e6d5b5c349211144bb9194b254d9bad62
John T Cronin <jt.cronin@outlook.com>
2017-09-22 13:12:04 -0500

    controllers are working

--- main.rb
+++ main.rb
@@ -4,6 +4,7 @@ require 'bundler'
 Bundler.require
 
 require_relative 'functions.rb'
+require_relative 'controllers/main_controller.rb'
 
 # TODO: Write some Ruby here that loops through models/ and for each Ruby file in that directory, it does `require_relative(...)` on that file.
 
